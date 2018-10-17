a12748e761201d75fb8dc443a1f6a17f287ba601
Tegomass <tegomassria@gmail.com>
2018-10-17 18:22:24 +0200

    update to timer

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,84 +1,160 @@
 import { Component, OnInit } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { AlertController } from 'ionic-angular';
 
 import { HttpClient } from '@angular/common/http';
 //import { HttpClientModule } from '@angular/common/http';
-
+import { LoadingController } from 'ionic-angular';
 
 import { Observable } from 'rxjs';
-import 'rxjs/add/operator/map'
+import 'rxjs/add/operator/map';
 import { Storage } from '@ionic/storage';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html',
-  styles: ['.tego-green { color: #2ecc71; }',
-    '.tego-red { color: #e74c3c; }']
+  styles: [ ]
 })
 export class HomePage implements OnInit {
- 
-  herokuUrl = "http://localhost:3000";
+
+  herokuUrl = 'http://localhost:3000';
   username = '';
   password = '';
-  disableSubmitButton = false;
-  message = '';
-  messageColor = '';
-  loading = false;
-  
-   onSubmit() {
-    if (this.username !== '' && this.password !== '') {
-      this.disableSubmitButton = true;
-      setTimeout(() => { this.disableSubmitButton = false }, 3000)
-    }
-    this.loading = true;
-    this.message = '';
-    this.sendRequest();
+  disableSubmitButton = true;
+  timer = 0;
+
+  onType() {
+    this.disableSubmitButton = this.username === '' || this.password === '';
     //console.log('submit', this.username, this.password)
   }
 
-  postToheroku(): Observable<any> {
-    const url = `${this.herokuUrl}/users?user=${this.username}&password=${this.password}`;
+  postToheroku(timer): Observable<any> {
+    const url = `${this.herokuUrl}/users?user=${this.username}&password=${this.password}&timer=${timer}`;
     return this.http.get(url);
   }
 
   /**
-   * Set callback message color
+   * A request to verify Credentials
    */
-  setMessageColor(code) {
-    if (code === 200) {
-      this.messageColor = 'tego-green';
-    } else {
-      this.messageColor = 'tego-red';
-    }
+  postToherokuVerify(timer): Observable<any> {
+    const url = `${this.herokuUrl}/users/credentials?user=${this.username}&password=${this.password}&timer=${timer}`;
+    return this.http.get(url);
   }
 
   /**
    * Store local data (like username and password)
    */
   store(event) {
-    //console.log(event.target.name, this[event.target.name])
-    this.storage.set(event.target.name, this[event.target.name]);
+    const element = event.srcElement.attributes.id || event.currentTarget.id;
+    console.log(element);
+    console.log(element, this[ element ]);
+    this.storage.set(element, this[ element ]);
   }
 
   /**
    * Send request to server
    */
   sendRequest() {
-    this.postToheroku()
+    this.postToheroku(this.timer)
       .subscribe(res => {
-        this.loading = false;
-        this.message = res.message;
-        this.setMessageColor(res.status);
-        console.log(res)
+        console.log(res);
+        if (res.status !== 200){
+          this.alertError(res.message);
+        }
       });
-  } 
-  
-  constructor(public navCtrl: NavController, private http: HttpClient, private storage: Storage) { }
+  }
+
+
+  constructor(private alertCtrl: AlertController, public loadingCtrl: LoadingController, private http: HttpClient, private storage: Storage) {
+  }
+
+  presentLoading() {
+    const loader = this.loadingCtrl.create({
+      content: 'Please wait...'
+    });
+    loader.present();
+    return loader;
+  }
+
+  alertConfirm(name) {
+    let alert = this.alertCtrl.create({
+      title: name,
+      message: `Slide for ${this.timer} minutes ?`,
+      enableBackdropDismiss: false,
+      buttons: [ {
+        text: 'Cancel',
+        role: 'cancel',
+        handler: () => {
+          console.log('Cancel clicked');
+        }
+      },
+        {
+          text: 'Confirm',
+          handler: () => {
+            console.log('Confirm clicked');
+
+            this.postToheroku(this.timer)
+              .subscribe(res => {
+                console.log(res);
+                if (res.status !== 200){
+                  this.alertError(res.message);
+                } else {
+                  this.alertSuccess();
+                }
+              });
+
+          }
+        } ]
+    });
+    alert.present();
+  }
+
+  alertError(message) {
+    let alert = this.alertCtrl.create({
+      title: 'Error',
+      message: message,
+      enableBackdropDismiss: false,
+      buttons: [ {
+        text: 'OK',
+        handler: () => {
+          console.log('Confirm clicked');
+        }
+      } ]
+    });
+    alert.present();
+  }
+
+  checkCredentials() {
+    let loadingPopup = this.presentLoading();
+    this.postToherokuVerify(this.timer)
+      .subscribe(res => {
+        let status = res.status;
+        // console.log(status);
+        // console.log(res);
+        loadingPopup.dismiss();
+        if (res.status === 200) {
+          this.alertConfirm(res.name);
+        } else {
+          this.alertError(res.message);
+        }
+      });
+  }
+
+  alertSuccess() {
+    let alert = this.alertCtrl.create({
+      title: 'Request sent !',
+      //subTitle: '10% of battery remaining',
+      buttons: [ 'Dismiss' ]
+    });
+    alert.present();
+  }
 
   ngOnInit() {
     //retrieve local data from Storage
-    this.storage.get('username').then((username) => this.username = username)
-    this.storage.get('password').then((password) => this.password = password)
+    Promise.all([ this.storage.get('username'), this.storage.get('password') ]).then(values => {
+      this.username = values[ 0 ];
+      this.password = values[ 1 ];
+      this.disableSubmitButton = this.username === '' || this.password === '';
+    });
   }
 
 }
